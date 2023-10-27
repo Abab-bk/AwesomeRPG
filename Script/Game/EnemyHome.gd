0e55ca03a474af9ce7b7465fde0a1d8405379631
Hey R You Are <aiaian0530@gmail.com>
2023-10-27 20:03:12 +0800

    更改玩家索敌行为

--- Script/Game/EnemyHome.gd
+++ Script/Game/EnemyHome.gd
@@ -27,3 +27,4 @@ func spawn_a_enemy() -> void:
     get_parent().call_deferred("add_child", new_enemy)
     # 在 Enemy 脚本中设置等级及其他属性，因为 data 需要时间读取并赋值
     new_enemy.global_position = Vector2(randi_range(-347, 2935), randi_range(-117, 2751))
+    Master.player.find_closest_enemy()
