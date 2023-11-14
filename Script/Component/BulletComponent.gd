09274d73c40e737cfa96edfbcb36f38f70618463
Hey R You Are <55276676+Abab-bk@users.noreply.github.com>
2023-11-14 21:50:56 +0800

    增加远程武器伤害

--- Script/Component/BulletComponent.gd
+++ Script/Component/BulletComponent.gd
@@ -1,5 +1,8 @@
 class_name BulletComponent extends CharacterBody2D
 
+@onready var hit_box:SimpleHitBoxComponent = $SimpleHitBoxComponent
+
+var buff_manager:FlowerBuffManager
 var target_pos:Vector2 = Vector2(0, 0)
 var speed:float = 1000.0
 
@@ -8,6 +11,8 @@ func _ready() -> void:
     collision_mask = 0
     
     set_collision_mask_value(4, true)
+    
+    hit_box.damage_data = buff_manager.output_data
     
     await get_tree().create_timer(1.0).timeout
     queue_free()
--- Script/Component/RangedWeaponComponent.gd
+++ Script/Component/RangedWeaponComponent.gd
@@ -1,6 +1,7 @@
 class_name RangedWeaponComponent extends Node2D
 
 @export var bullet:PackedScene
+@export var buff_manager:FlowerBuffManager
 
 var target_position:Vector2 = Vector2(0, 0)
 
@@ -8,6 +9,8 @@ func attack() -> void:
     var _new = bullet.instantiate() as BulletComponent
     _new.target_pos = target_position
     _new.global_position = global_position
+    _new.buff_manager = buff_manager
+    #SoundManager.play_sound(load(Master.CLICK_SOUNDS))
     Master.world.call_deferred("add_child", _new)
 
 func _physics_process(_delta:float) -> void:
