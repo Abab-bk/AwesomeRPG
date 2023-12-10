8f0fc91f7c0c46b41e87ef35f0b1d103126fdbff
Hey R You Are <55276676+Abab-bk@users.noreply.github.com>
2023-12-10 21:00:34 +0800

    添加更多传奇词条

--- src/Script/UI/ForgeAffix.gd
+++ src/Script/UI/ForgeAffix.gd
@@ -17,31 +17,35 @@ var locked:bool = false:
         else:
             icon.texture = load("res://Assets/UI/Icons/UnLock.svg")
 
+
 func _ready() -> void:
     lock_btn.pressed.connect(func():
         locked = !locked
         )
     update_ui("current_affix")
 
+
 func random_change_affix() -> void:
     forged_affix = Master.get_random_affix()
     update_ui("forged_affix")
+
 
 func clean() -> void:
     current_affix = null
     forged_affix = null
     update_ui("current_affix")
 
+
 func update_ui(_key:String) -> void:
     if _key == "current_affix":
         if not current_affix:
             desc_label.text = "空词缀"
             return
+        
         desc_label.text = current_affix.desc
+    
     if _key == "forged_affix":
         if not forged_affix:
-            print("forgeed_affix无效")
             return
-        print("forged_affix有效：", forged_affix.desc)
+        
         desc_label.text = forged_affix.desc
-        print("desc_label：", forged_affix.desc)
